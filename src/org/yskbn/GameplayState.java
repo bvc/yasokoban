b278e73985bc59e673daa33bd43819ff103e122d
bvc <brian.from.sf@gmail.com>
2012-02-28 20:41:37 -0800

    Testing out sprite drawing

--- src/org/yskbn/GameplayState.java
+++ src/org/yskbn/GameplayState.java
@@ -2,13 +2,17 @@ package org.yskbn;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GameplayState extends BasicGameState
 {
     private int stateID = -1;
+
+    private SpriteSheet sheet;
 
     public GameplayState(int stateID)
     {
@@ -24,18 +28,45 @@ public class GameplayState extends BasicGameState
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException
     {
+        sheet = new SpriteSheet("res/yasokoban_sprite.png", 32, 32);
+        // Make sprite position with 32 x 32 cells
+
+
+
 
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics graphics) throws SlickException
     {
+        sheet.getSprite(0,0).draw();
+        // draw sprite image from cell 0, 0
 
+        sheet.getSprite(1,0).draw(32, 0);
+        // draw sprite image from cell 1, 0
     }
 
     @Override
-    public void update(GameContainer container, StateBasedGame game, int i) throws SlickException
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException
     {
+        if (container.getInput().isKeyPressed(Input.KEY_LEFT))
+        {
+            System.out.println("LEFT");
+        }
+        
+        if (container.getInput().isKeyPressed(Input.KEY_UP))
+        {
+            System.out.println("UP");
+        }
 
+        if (container.getInput().isKeyPressed(Input.KEY_RIGHT))
+        {
+            System.out.println("RIGHT");
+        }
+
+        if (container.getInput().isKeyPressed(Input.KEY_DOWN))
+        {
+            System.out.println("DOWN");
+        }
     }
 }
